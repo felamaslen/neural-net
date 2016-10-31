fca72e29c9bd259dfa054f2492f820c308a6f85d
Fela Maslen <felamaslen@gmail.com>
2016-10-31 17:31:20 +0000

    Simulation skeleton

--- game.py
+++ game.py
@@ -1,9 +1,15 @@
+#!/usr/bin/python3
+
 from math import sin, cos, atan2, sqrt, pi
 from random import random
+import time
+
+ENV_DECISION_TIME = 0.5 # seconds
 
 ENV_WIDTH = 1000
 ENV_HEIGHT = 1000
 ENV_N_FOOD = 10
+ENV_N_ANIMALS = 5
 
 ANIMAL_MOVE_DISTANCE = 5
 
@@ -28,6 +34,13 @@ class Animal(Entity):
 
         self.food = food
 
+    def simulate(self):
+        """
+        this is run once per simulation
+        this function is the input point to the neural network belonging to this animal
+        """
+        pass
+
     def get_strongest_direction(self):
         """ returns the direction of the strongest smell """
         distance = [sqrt((self.x - food.x) ** 2, (self.y - food.y) ** 2) for food in self.food]
@@ -48,7 +61,7 @@ class Animal(Entity):
 class Food(Entity):
     """ defines a food particle in the environment (later add taste, health etc. """
     def __init__(self, x, y):
-        super(Animal, self).__init__(x, y)
+        super(Food, self).__init__(x, y)
 
         self.strength = FOOD_STRENGTH
 
@@ -62,8 +75,25 @@ class Environment(object):
         """ defines the location of food particles: array of Food instances """
         self.food = []
 
+        """ defines the animals in the environment """
+        self.generate_animals()
+
         """ generate random food to begin with """
         self.generate_food()
+
+    def generate(self):
+        self.num_simulations = 0
+
+        while True:
+            self.simulate()
+            self.num_simulations += 1
+            time.sleep(ENV_DECISION_TIME)
+
+    def simulate(self):
+        print("Simulation %s" % self.num_simulations)
+
+        for item in self.animals:
+            item.simulate()
 
     def generate_food(self):
         """ generates random food particles """
@@ -71,7 +101,15 @@ class Environment(object):
 
         pos = [(random() * self.W, random() * self.H) for i in range(num_food)]
 
-        self.food = [Food(x, y) for (x, y) in pos]
+        self.food += [Food(x, y) for (x, y) in pos]
+
+    def generate_animals(self):
+        """ generates random animals on start (only called once) """
+        num_animals = ENV_N_ANIMALS
+
+        pos = [(random() * self.W, random() * self.H) for i in range(num_animals)]
+
+        self.animals = [Animal(x, y, self.food) for (x, y) in pos]
 
 """ new environment """
 env = Environment()
