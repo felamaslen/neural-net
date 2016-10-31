5fb3f7b4bbc238bed9a8e684e3e36710b3906e4a
Fela Maslen <felamaslen@gmail.com>
2016-10-31 17:02:39 +0000

    distance smell

--- game.py
+++ game.py
@@ -7,6 +7,8 @@ ENV_N_FOOD = 10
 
 ANIMAL_MOVE_DISTANCE = 5
 
+FOOD_STRENGTH = 1
+
 class Entity(object):
     def __init__(self, x, y):
         """ the position of the entity in the environment """
@@ -15,11 +17,32 @@ class Entity(object):
 
 class Animal(Entity):
     """ defines an animal, which is a "species" of the neural network """
-    def __init__(self, x, y):
+    def __init__(self, x, y, food):
         super(Animal, self).__init__(x, y)
 
         """ the orientation of the animal in the environment (range: 0 to 2pi) """
         self.orientation = 0
+
+        """ this is where the magic happens """
+        self.neurons = []
+
+        self.food = food
+
+    def get_strongest_direction(self):
+        """ returns the direction of the strongest smell """
+        relative_pos_strength = [
+            (
+                sqrt(pow(self.x - food.x, 2) + pow(self.y - food.y, 2)),
+                atan((food.y - self.y) / (food.x - self.y))
+            )
+            for food in self.food
+        ]
+
+        smell = [1 / (pow(distance + 1, 2)) for (distance, angle) in relative_pos]
+
+        food_key = smell.index(max(smell))
+
+        return relative_pos_strength[food_key][1]
 
     def move(self, angle):
         """ turns and moves forward by a set distance """
@@ -33,6 +56,7 @@ class Food(Entity):
     def __init__(self, x, y):
         super(Animal, self).__init__(x, y)
 
+        self.strength = FOOD_STRENGTH
 
 class Environment(object):
     """ defines the environment for the simulation """
